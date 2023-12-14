f496c82fac1571d1aecdbac1da5e747b4f112a41
FreedomCrue <maxkuz777@list.ru>
2023-12-15 00:48:27 +0300

    123

---
+++ script.ts
@@ -0,0 +1,37 @@
+// Define an interface for the raccoon object
+interface Raccoon {
+  name: string;
+  weight: number;
+  age: number;
+}
+
+// Use an array of Raccoon type instead of any[]
+const ENOTS: Raccoon[] = [];
+
+function addRaccoon() {
+  // Use type assertions to tell TypeScript that these elements will exist
+  var name = (document.getElementById('name') as HTMLInputElement).value;
+  var weight = parseFloat((document.getElementById('weight') as HTMLInputElement).value);
+  var age = parseInt((document.getElementById('age') as HTMLInputElement).value);
+
+  // Check if the input values are valid before proceeding
+  if (!name || isNaN(weight) || isNaN(age)) {
+    alert('Please enter valid values for all fields.');
+    return;
+  }
+
+  var raccoon: Raccoon = {
+    name: name,
+    weight: weight,
+    age: age
+  };
+
+  ENOTS.push(raccoon);
+
+  var outputDiv = document.getElementById('output');
+  if (outputDiv) {
+    outputDiv.innerHTML = `Вы добавили "${raccoon.name}", вес ${raccoon.weight} кг, возраст ${raccoon.age} года.`;
+  }
+}
+
+
